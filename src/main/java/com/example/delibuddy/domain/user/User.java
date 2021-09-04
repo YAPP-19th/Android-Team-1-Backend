6aa3057b7879b405012c10730c4d3d21210674b4
triumph1 <seungwon.jeong@deliveryhero.co.kr>
2021-09-04 18:18:19 +0900

    create entity user and it's repository

---
+++ src/main/java/com/example/delibuddy/domain/user/User.java
@@ -0,0 +1,31 @@
+package com.example.delibuddy.domain.user;
+
+import com.example.delibuddy.domain.BaseTimeEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 20, nullable = false)
+    private String nickName;
+
+    @Column(length = 100, nullable = false, unique = true)
+    private String kakaoId;
+
+    @Builder
+    public User(String nickName, String kakaoId) {
+        this.nickName = nickName;
+        this.kakaoId = kakaoId;
+    }
+
+}
---
+++ src/test/java/com/example/delibuddy/domain/user/UserRepositoryTest.java
@@ -0,0 +1,40 @@
+package com.example.delibuddy.domain.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class UserRepositoryTest {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Test
+    public void BaseTimeEntity_등록() {
+        // Given
+        LocalDateTime now = LocalDateTime.of(2019,6,4,0,0,0);
+        userRepository.save(
+                User.builder()
+                .nickName("테스트 닉네임")
+                .kakaoId("테스트-카카오-id")
+                .build()
+        );
+
+        // When
+        List<User> userList = userRepository.findAll();
+
+        // Then
+        User user = userList.get(0);
+        System.out.println("user.nickName = " + user.getNickName());
+        System.out.println("user.kakaoId = " + user.getKakaoId());
+        System.out.println("user.ModifiedAt = " + user.getModifiedAt());
+        System.out.println("user.CreatedAt = " + user.getCreatedAt());
+    }
+}
