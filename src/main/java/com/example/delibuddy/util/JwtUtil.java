17ae522f6774ac0931d1870d486e1deac09540bf
triumph1 <seungwon.jeong@deliveryhero.co.kr>
2021-10-04 19:44:56 +0900

    재수정

--- src/main/java/com/example/delibuddy/util/JwtUtil.java
+++ src/main/java/com/example/delibuddy/util/JwtUtil.java
@@ -16,7 +16,7 @@ import java.util.function.Function;
 @Service
 public class JwtUtil {
 
-    @Value("jwt.secret")
+    @Value("${jwt.secret}")
     private String SECRET_KEY;
 
     public String extractUsername(String token) {
