fdcf72460d01bb2185f820812aeec00fe60858d8
Haejin <hj0816hj@gmail.com>
2021-10-30 22:07:28 +0900

    chore: add builder in CommentCreationRequestDto

--- src/main/java/com/example/delibuddy/web/dto/CommentCreationRequestDto.java
+++ src/main/java/com/example/delibuddy/web/dto/CommentCreationRequestDto.java
@@ -3,9 +3,12 @@ package com.example.delibuddy.web.dto;
 import com.example.delibuddy.domain.comment.Comment;
 import com.example.delibuddy.domain.party.Party;
 import com.example.delibuddy.domain.user.User;
+import lombok.Builder;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
+
+@Builder
 @Data
 @RequiredArgsConstructor
 public class CommentCreationRequestDto {
