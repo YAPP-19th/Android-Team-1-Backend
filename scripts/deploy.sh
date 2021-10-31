16d7a2352aa74bffbdb496460f517d934baa5421
Haejin <hj0816hj@gmail.com>
2021-11-01 00:05:56 +0900

    fix: change jar path

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,7 @@ PROJECT_NAME=Android-Team-1-Backend
 
 echo "> Build 파일 복사"
 
-cp $REPOSITORY/zip/*.jar $REPOSITORY/
+cp $REPOSITORY/zip/build/libs/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
