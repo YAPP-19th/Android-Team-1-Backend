93d53e324ff0b2134b32649dfe9575e36f7beeb0
Haejin <hj0816hj@gmail.com>
2021-11-06 22:31:52 +0900

    fix: change parameter order

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -33,5 +33,4 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
-nohup java -jar --spring.config.location=/home/ubuntu/app/application.yml \
-    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar $JAR_NAME --spring.config.location=/home/ubuntu/app/application.yml > $REPOSITORY/nohup.out 2>&1 &
