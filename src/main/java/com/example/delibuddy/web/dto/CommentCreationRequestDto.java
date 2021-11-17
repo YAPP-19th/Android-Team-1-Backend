2baceac4abd153908b059ac20e551208f44e9043
Haejin <hj0816hj@gmail.com>
2021-11-18 00:36:38 +0900

    fix: add constructor to CommentCreationRequestDto

--- src/main/java/com/example/delibuddy/web/dto/CommentCreationRequestDto.java
+++ src/main/java/com/example/delibuddy/web/dto/CommentCreationRequestDto.java
@@ -3,6 +3,7 @@ package com.example.delibuddy.web.dto;
 import com.example.delibuddy.domain.comment.Comment;
 import com.example.delibuddy.domain.party.Party;
 import com.example.delibuddy.domain.user.User;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -12,6 +13,13 @@ public class CommentCreationRequestDto {
     private String body;
     private Long partyId;
     private Long parentId;
+
+    @Builder
+    public CommentCreationRequestDto(String body, Long partyId, Long parentId) {
+        this.body = body;
+        this.partyId = partyId;
+        this.parentId = parentId;
+    }
 
     public Comment toEntity(User writer, Party party) {
         return Comment.builder()
