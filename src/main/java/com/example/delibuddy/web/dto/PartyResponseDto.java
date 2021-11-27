fc5dc8febda0ce131a17462ad61cbd387e247567
triumph1 <seungwon.jeong@deliveryhero.co.kr>
2021-11-27 16:12:02 +0900

    remove redundant serializer

--- src/main/java/com/example/delibuddy/web/dto/PartyResponseDto.java
+++ src/main/java/com/example/delibuddy/web/dto/PartyResponseDto.java
@@ -1,32 +1,18 @@
 package com.example.delibuddy.web.dto;
 
-import com.example.delibuddy.domain.category.Category;
 import com.example.delibuddy.domain.party.Party;
-import com.example.delibuddy.domain.user.User;
-import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.databind.JsonSerializer;
-import com.fasterxml.jackson.databind.SerializerProvider;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.stream.Collectors;
-
-
 
 @Data
-@RequiredArgsConstructor // TODO 없애기
+@RequiredArgsConstructor
 public class PartyResponseDto {
     private final long id;
     private final String title;
     private final String body;
     private final String coordinate;
-
-    @JsonSerialize(using = CategorySerializer.class)
     private final CategoryResponseDto category;
-
     private final Integer targetUserCount;
 
     public PartyResponseDto(Party entity) {
@@ -36,15 +22,5 @@ public class PartyResponseDto {
         coordinate = "POINT (" + entity.getCoordinate().getX() + " " + entity.getCoordinate().getY() + ")"; // TODO: coordinate 에 예시 넣기
         category = new CategoryResponseDto(entity.getCategory());
         targetUserCount = entity.getTargetUserCount();
-    }
-}
-
-class CategorySerializer extends JsonSerializer<Category> {
-    @Override
-    public void serialize(Category value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
-        gen.writeStartObject();
-        gen.writeNumberField("id", value.getId());
-        gen.writeStringField("name", value.getName());
-        gen.writeEndObject();
     }
 }
