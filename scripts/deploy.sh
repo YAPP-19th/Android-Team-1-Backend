e62b955031b5385a5b62462d6c0c9b579e15c7aa
Haejin <hj0816hj@gmail.com>
2021-12-09 23:24:46 +0900

    feat: add create symlink command

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,6 +6,10 @@ echo "> Build 파일 복사"
 
 cp $REPOSITORY/zip/build/libs/*.jar $REPOSITORY/
 
+echo "> 심볼릭 링크 연결(덮어쓰기)"
+
+ln -sf $(ls -tr $REPOSITORY/*.jar | tail -n 1) delibuddy.jar
+
 echo "> 서비스 재시작"
 
 systemctl restart delibuddy.service
