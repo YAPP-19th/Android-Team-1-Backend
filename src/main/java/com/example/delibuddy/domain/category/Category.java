5e9071e6c5cddf93f2876147d28d0eebbf1a1598
Haejin <hj0816hj@gmail.com>
2021-12-10 00:26:14 +0900

    feat: add iconUrl, backgroundColorCode field in category entity

--- src/main/java/com/example/delibuddy/domain/category/Category.java
+++ src/main/java/com/example/delibuddy/domain/category/Category.java
@@ -21,9 +21,17 @@ public class Category extends BaseTimeEntity {
     @Column(length = 30, unique = true)
     private String code;
 
-    public Category(String name, String code) {
+    @Column(length = 200)
+    private String iconUrl;
+
+    @Column(length = 30)
+    private String backgroundColorCode;
+
+    public Category(String name, String code, String iconUrl, String backgroundColorCode) {
         this.name = name;
         this.code = code;
+        this.iconUrl = iconUrl;
+        this.backgroundColorCode = backgroundColorCode;
     }
 
 }
