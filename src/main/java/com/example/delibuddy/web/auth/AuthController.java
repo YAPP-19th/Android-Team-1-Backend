478511a0fef3255b87c0039a8729772922f8950a
triumph1 <seungwon.jeong@deliveryhero.co.kr>
2021-12-19 19:11:43 +0900

    new api, refresh jwt

--- src/main/java/com/example/delibuddy/web/auth/AuthController.java
+++ src/main/java/com/example/delibuddy/web/auth/AuthController.java
@@ -11,6 +11,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -29,7 +30,7 @@ public class AuthController {
     @Autowired
     private JwtUtil jwtUtil;
 
-    @RequestMapping(value = "/authenticate", method = RequestMethod.POST) // PostMapping 으로 바꾸기
+    @RequestMapping(value = "${api.v1}/auth", method = RequestMethod.POST)
     public AuthenticationResponseDto createAuthenticationToken(@RequestBody AuthenticationRequestDto authenticationRequestDto) throws Exception {
         KakaoMyInfo kakaoMyinfo;
 
@@ -44,6 +45,14 @@ public class AuthController {
 
         final String jwt = jwtUtil.generateToken(user.getKakaoId());
 
+        return new AuthenticationResponseDto(jwt, user.getId());
+    }
+
+    @RequestMapping(value = "${api.v1}/auth/refresh", method = RequestMethod.POST)
+    public AuthenticationResponseDto refreshAuthenticationToken() {
+        MyUserDetails userDetails = (MyUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User user = userRepository.findByKakaoId(userDetails.getUsername()).get();
+        final String jwt = jwtUtil.generateToken(user.getKakaoId());
         return new AuthenticationResponseDto(jwt, user.getId());
     }
 }
